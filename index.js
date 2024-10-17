8558652e901df5deee192ff43fb106c0b29392ab
whoisPratish <pratish.dhakal2059@gmail.com>
2024-10-17 18:47:55 -0400

    connecting to database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-
+// mongodb+srv://madridistapratish7:<db_password>@fullstack-blog.kgce4.mongodb.net/?retryWrites=true&w=majority&appName=fullstack-blog
+// pratish
 const PORT = process.env.PORT || 3000;
 
 
